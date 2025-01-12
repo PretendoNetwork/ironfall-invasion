c48adda43369f22e93825c200cc63736ab7f0288
Andrea Toska <toskaandrea@gmail.com>
2025-01-12 11:46:58 +0100

    fix(globals): forgot to update error

--- globals/get_user_friend_pids.go
+++ globals/get_user_friend_pids.go
@@ -3,7 +3,7 @@ package globals
 import (
 	"context"
 
-	pb_friends "github.com/PretendoNetwork/grpc-go/friends"
+	pbfriends "github.com/PretendoNetwork/grpc-go/friends"
 	"github.com/PretendoNetwork/nex-protocols-go/v2/globals"
 	"google.golang.org/grpc/metadata"
 )
@@ -11,7 +11,7 @@ import (
 func GetUserFriendPIDs(pid uint32) []uint32 {
 	ctx := metadata.NewOutgoingContext(context.Background(), GRPCFriendsCommonMetadata)
 
-	response, err := GRPCFriendsClient.GetUserFriendPIDs(ctx, &pb_friends.GetUserFriendPIDsRequest{Pid: pid})
+	response, err := GRPCFriendsClient.GetUserFriendPIDs(ctx, &pbfriends.GetUserFriendPIDsRequest{Pid: pid})
 	if err != nil {
 		globals.Logger.Error(err.Error())
 		return make([]uint32, 0)
--- globals/password_from_pid.go
+++ globals/password_from_pid.go
@@ -3,19 +3,20 @@ package globals
 import (
 	"context"
 
-	"github.com/PretendoNetwork/nex-go/v2/types"
 	pb_account "github.com/PretendoNetwork/grpc-go/account"
 	"github.com/PretendoNetwork/nex-go/v2"
+	"github.com/PretendoNetwork/nex-go/v2/types"
+	"github.com/PretendoNetwork/nex-protocols-go/v2/globals"
 	"google.golang.org/grpc/metadata"
 )
 
 func PasswordFromPID(pid *types.PID) (string, uint32) {
 	ctx := metadata.NewOutgoingContext(context.Background(), GRPCAccountCommonMetadata)
 
-	response, err := GRPCAccountClient.GetNEXPassword(ctx, &pb_account.GetNEXPasswordRequest{Pid: pid.LegacyValue()	})
+	response, err := GRPCAccountClient.GetNEXPassword(ctx, &pb_account.GetNEXPasswordRequest{Pid: pid.LegacyValue()})
 	if err != nil {
 		globals.Logger.Error(err.Error())
-		return "", nex.Errors.RendezVous.InvalidUsername
+		return "", nex.ResultCodes.RendezVous.InvalidUsername
 	}
 
 	return response.Password, 0
